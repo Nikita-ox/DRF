c55a60e8cada65dbfdc1b3c346d58fd1fc4f5d33
NIK <nikita1z@mail.ru>
2022-11-30 18:14:11 +0300

    ListCreateAPIView

--- src/stars/serializers.py
+++ src/stars/serializers.py
@@ -3,6 +3,7 @@ import io
 from rest_framework import serializers
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
+from rest_framework.serializers import ModelSerializer
 
 from stars.models import Stars
 
@@ -13,25 +14,21 @@ from stars.models import Stars
 #         self.content = content
 
 
-class StarsSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=2000)
-    content = serializers.CharField()
-    time_create = serializers.DateTimeField(read_only=True)
-    time_update = serializers.DateTimeField(read_only=True)
-    is_published = serializers.BooleanField(default=True)
-    cat_id = serializers.IntegerField()  # ForeignKey перобразовывается
+class StarsSerializer(ModelSerializer):
+    class Meta:
+        model = Stars
+        fields = "__all__"
 
-# def encode():
-#     model = StarsModel('Angelina Jolie', 'Kristian Stuart')
-#     model_sr = StarsSerializer(model)
-#     print(model_sr.data, sep='\n')
-#     json = JSONRenderer().render(model_sr.data)
-#     print(json)
-#
-#
-# def decode():
-#     stream = io.BytesIO(b'{"title":"Angelina Jolie","content":"Kristian Stuart"}')
-#     data = JSONParser().parse(stream)
-#     serializers = StarsSerializer(data=data)
-#     serializers.is_valid()
-#     print(serializers.validated_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
