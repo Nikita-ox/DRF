499f0abb554496ce6a33da2cb330be3040c3de4a
NIK <nikita1z@mail.ru>
2022-12-11 22:14:53 +0300

    Авторизация и аутентификация | Аутентификация по токенам. Пакет Djoser

--- src/about_stars/settings.py
+++ src/about_stars/settings.py
@@ -27,6 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'stars.apps.StarsConfig',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+
 ]
 
 MIDDLEWARE = [
@@ -111,12 +114,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        # 'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
         # Доступ к данным предоставлен только авторизованным пользователям
         # Нужно поставить в комментарий или удалить permission_classes = [IsAuthenticatedOrReadOnly]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+
     ]
 }
-
--- src/about_stars/urls.py
+++ src/about_stars/urls.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 from stars import views
 # from stars.views import ListViews
 from stars.views import *
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),  # Авторизация на основе сесии
     path('api/v1/stars/', StarsAPIList.as_view()),
     path('api/v1/stars/<int:pk>/', StarsAPIUpdate.as_view()),
     path('api/v1/starsdelete/<int:pk>/', StarsAPIDestroy.as_view()),
+    path('api/v1/auth/', include("djoser.urls")),
+    re_path(r'^auth/', include("djoser.urls.authtoken"))  # Авторизация по токену
 ]
+
 
